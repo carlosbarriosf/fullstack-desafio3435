b60fe65484b9a8bb3a20f004872b5c7a2f71ef99
Carlos Barrios <carlosbarriosf6@gmail.com>
2024-05-20 18:49:52 -0300

    added middlewares

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,13 +1,71 @@
 import express from "express"
 import { deleteNoteById, getNote, getNoteById, postNote, updateNoteById } from "../controllers/notesController.js";
+import { body, param } from "express-validator";
+import { validationErrorResponse } from "../middlewares/validations.js";
 
 const route = express.Router();
 
 route
-    .post("/", postNote)
+    .post(
+        "/",
+        [
+            body('title')
+                .exists()
+                .withMessage('El título es obligatorio')
+                .bail()
+                .isString()
+                .withMessage('El título debe ser de tipo string')
+                .bail()
+                .isLength({min: 1})
+                .withMessage('El título no puede estar vacío'),
+            body('description')
+                .exists()
+                .withMessage('La descripción es obligatoria')
+                .bail()
+                .isString()
+                .withMessage('La descripción debe ser de tipo string')
+                .bail()
+                .isLength({min: 3})
+                .withMessage('La descripción debe tener al menos 3 caracteres'),
+            validationErrorResponse
+        ] ,
+        postNote
+    )
     .get("/", getNote)
-    .get("/:id", getNoteById)
-    .put("/update/:id", updateNoteById)
-    .delete("/delete/:id", deleteNoteById)
+    .get(
+        "/:id", 
+        [
+            param('id').isMongoId().withMessage('El id de la nota no es válido'),
+            validationErrorResponse
+        ],
+        getNoteById)
+    .put(
+        "/update/:id",
+        [
+            param('id').isMongoId().withMessage('El id de la nota no es válido'),
+            body('title')    
+                .optional()
+                .isString()
+                .withMessage('El título debe ser de tipo string')
+                .bail()
+                .isLength({min: 1})
+                .withMessage('El título no puede estar vacío'),
+            body('description')
+                .optional()
+                .isString()
+                .withMessage('La descripción debe ser de tipo string')
+                .bail()
+                .isLength({min: 3})
+                .withMessage('La descripción debe tener al menos 3 caracteres'),
+                validationErrorResponse
+        ], 
+        updateNoteById)
+    .delete(
+        "/delete/:id", 
+        [
+            param('id').isMongoId().withMessage('El id de la nota no es válido'),
+            validationErrorResponse
+        ],
+        deleteNoteById)
 
 export default route
